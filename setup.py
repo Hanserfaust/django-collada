f54b9522c950cb6eeea9716c10f141f8a12bdf7c
Hans Eklund <hans.eklund@igw.se>
2021-03-20 11:27:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='django-collada',
-    version='0.80',
+    version='0.81',
     description='A Django application that adds support for uploading and visualizing a Collada model.',
     author='Hans Eklund',
     author_email='hans.eklund@igw.se',
