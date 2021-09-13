5fbd87622e17a709451472c63b250f23b168420b
Hanserfaust <hans.eklund@igw.se>
2021-09-13 09:02:29 +0200

    Migrations package in setup.py now.

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@
 from distutils.core import setup
 
 setup(name='django-collada',
-    version='0.82',
+    version='1.0.2',
     description='A Django application that adds support for uploading and visualizing a Collada model.',
     author='Hans Eklund',
     author_email='hans.eklund@igw.se',
     url='https://github.com/iGW/django-collada',
-    packages=['django_collada'],
+    packages=[
+        'django_collada',
+        'django_collada.migrations'
+    ],
     requirements=[
         'pycollada==0.4.1'
     ]
