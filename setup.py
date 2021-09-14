34a9f7bd1268691ea717f2a71776cfe78bd75761
Hanserfaust <hans.eklund@igw.se>
2021-09-14 10:29:53 +0200

    Removed usage of django.conf.urls.patterns().

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='django-collada',
-    version='1.0.2',
+    version='1.0.3',
     description='A Django application that adds support for uploading and visualizing a Collada model.',
     author='Hans Eklund',
     author_email='hans.eklund@igw.se',
