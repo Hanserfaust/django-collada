c2971790c75e3da2d7d8c2791de9af0fb66f6e1d
Hanserfaust <hans.eklund@igw.se>
2021-09-15 16:39:19 +0200

    Removed ref to patterns for later Django comp.

--- django_collada/urls.py
+++ django_collada/urls.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from django_collada.views import home, three_js
 from django_collada.services import get_foobar_properties
 
 
-urlpatterns = patterns('django_collada.views',
+urlpatterns = [
     url(r'^django_collada/$', home, name='django_collada_home'),
     url(r'^django_collada/three_js/$', three_js, name='django_collada_three_js'),
-)
+]
 
 urlpatterns += [
     url(r'^django_collada/get_foobar_properties/(?P<foobar_scene_id>[a-zA-Z0-9]+)/$', get_foobar_properties, name='django_collada_get_foobar_properties'),
